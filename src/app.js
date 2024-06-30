bb4492fd6516344977b4062d6655f476f052facd
Juan Pablo Menéndez Rodríguez <pablomero@gmail.com>
2024-06-30 00:25:17 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function rnd(str) {
+    return str[Math.floor(Math.random() * str.length)];
+  }
+  document.getElementById("excuse").innerHTML = `${rnd(who)} ${rnd(
+    action
+  )} ${rnd(what)} ${rnd(when)} `;
 };
